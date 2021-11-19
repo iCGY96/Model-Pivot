e6ae68a82ee010cf68144313bf31d74ed99a93be
Guangyao Chen <gy.chen@foxmail.com>
2021-11-19 13:11:13 +0800

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -2,9 +2,9 @@
 
 # -- Project information
 
-project = 'Lumache'
-copyright = '2021, Graziella'
-author = 'Graziella'
+project = 'Model-Pivot'
+copyright = '2021, Guangyao Chen'
+author = 'Guangyao Chen'
 
 release = '0.1'
 version = '0.1.0'
